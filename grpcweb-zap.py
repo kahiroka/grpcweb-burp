2d99d12159eee29dead490e987d0697eae49f930
kahiroka <kahiroka@users.noreply.github.com>
2025-08-12 21:17:29 +0900

    fix zap script

--- grpcweb-zap.py
+++ grpcweb-zap.py
@@ -1,27 +1,34 @@
 import json
-from protobufs import ProtoBufs
+import struct
+from protobufs import ProtoBufs # or inline here
 
 def decode_grpcweb(msg):
     msg.getRequestHeader().setHeader("x-grpcweb-flag", "true")
-    body = msg.getRequestBody()
+    body = msg.getRequestBody().getBytes().tostring()
     decoded = json.dumps(ProtoBufs.decode(body[5:]))
     msg.setRequestBody(decoded)
     msg.getRequestHeader().setContentLength(len(decoded))
 
 def encode_grpcweb(msg):
-    msg.getRequestHeader().removeHeader("x-grpcweb-flag")
-    body = msg.getRequestBody().toString()
+    msg.getRequestHeader().setHeader("x-grpcweb-flag", None)
+    body = msg.getRequestBody().getBytes().tostring()
     encoded = ProtoBufs.encode(json.loads(body))
-    msg.setRequestBody(encoded)
-    msg.getRequestHeader().setContentLength(len(encoded))
+    grpcweb = struct.pack("B", 0x00) + struct.pack(">I", len(encoded)) + encoded
+    msg.setRequestBody(grpcweb)
+    msg.getRequestHeader().setContentLength(len(grpcweb))
 
 def proxyRequest(msg):
+    print('proxyRequest called for url=' + msg.getRequestHeader().getURI().toString())
     header_value = msg.getRequestHeader().getHeader("content-type")
     if header_value and header_value.lower() == "application/grpc-web+proto":
         decode_grpcweb(msg)
     return True
 
+def proxyResponse(msg):
+    return True
+
 def sendingRequest(msg, initiator, helper):
+    print('sendingRequest called for url=' + msg.getRequestHeader().getURI().toString())
     header_value = msg.getRequestHeader().getHeader("x-grpcweb-flag")
     if header_value and header_value.lower() == "true":
         encode_grpcweb(msg)
